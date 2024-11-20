f4bf03288e05e2dc7653aa051572cde460abf502
Jihyun <dns12134@gmail.com>
2024-11-20 20:49:36 +0900

    chore: 정적데이터 입력 및 전역상태관리 수정

--- minjock-of-taeckbae/src/store.js
+++ minjock-of-taeckbae/src/store.js
@@ -1,45 +1,57 @@
 import { create } from 'zustand';
 
 export const useInputFormStore = create((set) => ({
-  isComparisonButtonClick: false,
-  moveQuestion: () => set({ isComparisonButtonClick: true }),
-  handleGoLandingPageClick: () => set({ isComparisonButtonClick: false }),
-  isNextVolume: false,
-  handleNextToVolumeClick: (e) => {
-    if (!Number(e.target.value)) {
-      set({ isWeightAlertModalOpen: true })
-      set({ weight: "" })
-    } else {
-      set({ isNextVolume: true, isComparisonButtonClick: false })
-    }
-  },
-  handleGoWeightInputClick: () => set({ isNextVolume: false, isComparisonButtonClick: true }),
-  handleGoCountryInputClick: (e) => {
-    let volumeArray;
-    let volumeInput = e.target.value;
-    if (!volumeInput || !volumeInput.includes("x") && !volumeInput.includes(",")) {
-      set({ isVolumeAlertModalOpen: true })
-    } else {
-      if (volumeInput.includes("x")) {
-        volumeArray = [...e.target.value.split("x")]
-      } else if (volumeInput.includes(",")) {
-        volumeArray = [...e.target.value.split(",")]
-      }
-      set({ volumeWeight: (Number(volumeArray[0]) * Number(volumeArray[1]) * Number(volumeArray[2]) / 6000) })
-      set({ isNextCountry: true, isNextVolume: false })
-    }
-  }
-  ,
-  handleGoVolumeInputClick: () => set({ isNextCountry: false, isNextVolume: true }),
   weight: "",
   isWeightAlertModalOpen: false,
   isVolumeAlertModalOpen: false,
+  selectedCountry: "",
+  isWeightover: false,
+  width: "",
+  height: "",
+  length: "",
+  landingPage: true,
+  isNextCountry: false,
+  isComparison: false,
+  isNextWeightAndVolume: false,
+  firstVolumeWeight: 0,
+  secondVolumeWeight: 0,
+  handlegoCountryInputClick: () => set({ landingPage: false, isNextCountry: true }),
+  handleGoLandingPageClick: () => set({ landingPage: true, isNextWeightAndVolume: false, isNextCountry: false, weight: "" }),
+  handleGoWeightInputClick: () => set({ isComparison: false, isNextWeightAndVolume: true, landingPage: false, isNextCountry: false }),
   handleWeightInputChange: (e) => {
     set({ weight: e.target.value })
   },
-  handlemodalCloseClick: () => set({ isWeightAlertModalOpen: false, isVolumeAlertModalOpen: false }),
-  volumeWeight: "",
-  selectedCountry: "",
+  handleWidthChange: (e) => {
+    set({ width: e.target.value })
+  },
+  handleLengthChange: (e) => {
+    set({ length: e.target.value })
+  },
+  handleHeightChange: (e) => {
+    set({ height: e.target.value })
+  },
+  handleGoComparisonClick: (e) => {
+    let inputArray = e.target.value.split(",");
+    let weight = Number(inputArray[0]);
+    let width = Number(inputArray[1]);
+    let length = Number(inputArray[2]);
+    let height = Number(inputArray[3]);
+
+    if (!weight || !width || !length || !height) {
+      set({ isWeightAlertModalOpen: true })
+      set({ weight: "", width: "", length: "", height: "" })
+    } else if (Number(inputArray[0]) > 20) {
+      set({ isWeightover: true })
+      set({ weight: "" })
+    } else if (width > 150 || (length * 2 + height * 2) > 300) {
+      set({ isVolumeAlertModalOpen: true })
+    } else {
+      set({ firstVolumeWeight: parseFloat((width * length * height / 6000).toFixed(1)) });
+      set({ secondVolumeWeight: parseFloat((width * length * height / 5000).toFixed(1)) });
+      set({ isComparison: true, landingPage: false, isNextWeightAndVolume: false })
+    }
+  },
+  handlemodalCloseClick: () => set({ landingPage: false, isWeightAlertModalOpen: false, isNextCountry: false, isVolumeAlertModalOpen: false, isWeightover: false }),
   handleSelectedCountryChange: (e) => set({ selectedCountry: e.value }),
 }));
 
