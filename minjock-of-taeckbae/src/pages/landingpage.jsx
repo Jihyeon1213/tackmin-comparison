5a8343a472a3dcd7ea17968c19d93025ec5f7db5
Jihyun <dns12134@gmail.com>
2024-12-17 13:03:47 +0900

    chore: 랜딩페이지 UI 수정

--- minjock-of-taeckbae/src/pages/landingpage.jsx
+++ minjock-of-taeckbae/src/pages/landingpage.jsx
@@ -1,7 +1,7 @@
 import useInputFormStore from "../store.js";
-import CountryInputForm from "../components/countryinputform.jsx";
 import ServiceComparison from "./serviceComparison.jsx";
 import WeightAndVolumeInputForm from "../components/weightAndVolumeInputForm.jsx";
+import CountryInputForm from "../components/countryInputForm.jsx";
 
 function LandingPage() {
   const {
@@ -23,21 +23,29 @@ function LandingPage() {
       )}
       {!isNextWeightAndVolume && !isNextCountry && landingPage && (
         <>
-          <div className="flex ml-10 mt-8 items-center">
-            <div className="mt-6 text-6xl ml-32">
-              택배<span className="text-5xl">의</span>민족
+          <div className="relative w-full flex justify-center items-center mb-8">
+            <div className="text-6xl font-bold text-center space-x-2">
+              <span className="text-blue-500">택배</span>
+              <span className="text-5xl">의</span>
+              <span className="text-yellow-400">민족</span>
             </div>
-            <img src="/src/taeckmin.png" className="w-24" />
+            <img
+              src="/src/taeckmin.png"
+              className="absolute right-0 w-16 h-16"
+              alt="택배의 민족 캐릭터"
+            />
           </div>
-          <div className="mt-5 justify-items-center">
-            <div className="mt-12 text-4xl">가장 빠르고</div>
-            <div className="text-4xl">저렴한 가격의</div>
-            <div className="mt-10 text-6xl text-blue-600">국제 택배</div>
-            <div className="mt-10 text-4xl">지금 찾아드릴게요</div>
-            <div className="mt-20 mb-8">
+          <div className="text-center space-y-10">
+            <div className="text-4xl font-semibold">가장 빠르고</div>
+            <div className="text-4xl font-semibold">저렴한 가격의</div>
+            <div className="text-6xl lg:text-6xl text-blue-400 font-bold">
+              국제 택배
+            </div>
+            <div className="text-4xl font-light mt-4">지금 찾아드릴게요</div>
+            <div className="mt-16">
               <button
                 onClick={handlegoCountryInputClick}
-                className=" bg-black text-white text-3xl p-2 rounded hover:bg-blue-600"
+                className=" bg-blue-600 text-white text-3xl p-4 rounded-lg hover:bg-yellow-500 transition-transform duration-300 ease-in-out transform hover:scale-105"
               >
                 한 번에 비교하기
               </button>
