cc7aa81e9513768e22c6e95d8dc17ba92aa5bbd1
Jihyun <dns12134@gmail.com>
2024-12-21 12:30:21 +0900

    feat: Tailwind에 슬라이드 애니메이션 추가

--- minjock-of-taeckbae/tailwind.config.js
+++ minjock-of-taeckbae/tailwind.config.js
@@ -7,7 +7,22 @@ export default {
         '128': '32rem',
         '144': '36rem',
       },
+      keyframes: {
+        slideInFromRight: {
+          '0%': { transform: 'translateX(100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+        slideInfromLeft: {
+          '0%': { transform: 'translateX(-100%)', opacity: '0' },
+          '100%': { transform: 'translateX(0)', opacity: '1' },
+        },
+      },
+      animation: {
+        slideInFromRight: 'slideInFromRight 1s ease-out',
+        slideInfromLeft: 'slideInfromLeft 1s ease-out',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+  ],
 };
